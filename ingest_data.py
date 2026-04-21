eccec4638f28127bcde677231859b920a0b67f41
James-al7 <James-al7@outlook.com>
2026-04-20 20:07:01 -0500

    n

---
+++ ingest_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine(
+    "postgresql+psycopg2://postgres:postgres@localhost:5432/instacart"
+)
+
+files = {
+    "orders": "data_ingestion/rawfiles/orders.csv",
+    "products": "data_ingestion/rawfiles/products.csv",
+    "aisles": "data_ingestion/rawfiles/aisles.csv",
+    "departments": "data_ingestion/rawfiles/departments.csv",
+
+}
+
+for table, path in files.items():
+    df = pd.read_csv(path)
+    df.to_sql(f"raw_{table}", engine, schema="raw", if_exists="replace", index=False)
+    print(f"Loaded {table}")
